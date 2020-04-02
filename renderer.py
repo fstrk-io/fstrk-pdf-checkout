a6d4bba87f9d452db5a52bf45a642f5cd3d4faa4
Mike <mikhail.g.novikov@gmail.com>
2020-04-02 19:54:40 +0300

    добавил задел для фильтров jinja

--- renderer.py
+++ renderer.py
@@ -1,6 +1,5 @@
-from datetime import datetime
-
-from jinja2 import Template
+from jinja2 import FileSystemLoader
+from jinja2 import Environment, select_autoescape
 from uuid import uuid4
 from weasyprint import HTML
 from datetime import datetime
@@ -13,11 +12,21 @@ from botocore.exceptions import ClientError
 import os
 
 
+def super_filter(value):
+    return f'SUPER FILTER!!!! {value} !!! SUPER FILTER!! '
+
+
+env = Environment(
+    loader=FileSystemLoader("./templates"),
+    autoescape=select_autoescape(["html", "xml"]),
+)
+
+env.filters["super_filter"] = super_filter
+
 
 def render_pdf(params: dict, out_file):
 
-    with open('./templates/order-1c.html') as f:
-        template = Template(f.read())
+    template = env.get_template("order-1c.html")
 
     result = template.render(**params)
 
@@ -27,8 +36,8 @@ def render_pdf(params: dict, out_file):
 def _get_current_time_str():
 
     utcnow = datetime.utcnow()
-    local = utcnow.replace(tzinfo=timezone('UTC')).astimezone(tz.gettz('Europe/Moscow'))
-    local_str = local.isoformat().split('.')[0]
+    local = utcnow.replace(tzinfo=timezone("UTC")).astimezone(tz.gettz("Europe/Moscow"))
+    local_str = local.isoformat().split(".")[0]
     return local_str
 
 
@@ -36,15 +45,15 @@ def upload_file(file_name: str) -> str:
     """ Загрузить файл на амазон, вернуть линк с уникальным именем """
 
     current_time_str = _get_current_time_str()
-    path_name = f'1c-orders/{uuid4()}-{current_time_str}.pdf'
+    path_name = f"1c-orders/{uuid4()}-{current_time_str}.pdf"
     # Upload the file
-    bucket_name = os.environ['AWS_S3_BUCKET_NAME']
-    s3_client = boto3.client('s3')
+    bucket_name = os.environ["AWS_S3_BUCKET_NAME"]
+    s3_client = boto3.client("s3")
     try:
         s3_client.upload_file(file_name, bucket_name, path_name)
     except ClientError as e:
         logging.error(e)
         return None
 
-    url = f'https://{bucket_name}.s3.amazonaws.com/{path_name}'
+    url = f"https://{bucket_name}.s3.amazonaws.com/{path_name}"
     return url
